aa23c2a1ea1f573499841c302e7f4acdb1e15e2b
frankaging <zxw0424@gmail.com>
2021-09-24 02:01:24 -0700

    minor

--- code/run_mid_tuning.py
+++ code/run_mid_tuning.py
@@ -56,6 +56,7 @@ from vocab_mismatch_utils import *
 logger = logging.getLogger(__name__)
 MODEL_CONFIG_CLASSES = list(MODEL_FOR_MASKED_LM_MAPPING.keys())
 MODEL_TYPES = tuple(conf.model_type for conf in MODEL_CONFIG_CLASSES)
+from functools import partial
 
 
 # In[13]:
