c1f7552ac4bc6ebd0c3785abb616207d9d9b2ab5
frankaging <zxw0424@gmail.com>
2021-10-17 21:41:59 -0700

    update

--- code/run_fine_tuning.py
+++ code/run_fine_tuning.py
@@ -560,10 +560,13 @@ if __name__ == "__main__":
         # we need to rewrite the number of type token a little
         # during pretraining, there are two types for reberta
         # during fine-tuning, i think we are only using one?
-        if args.tokenizer_name == "albert-base-v2":
-            config.type_vocab_size = 1
+        if os.path.isdir(args.model_name_or_path):
+            pass
         else:
-            config.type_vocab_size = 1
+            if args.tokenizer_name == "albert-base-v2":
+                config.type_vocab_size = 1
+            else:
+                config.type_vocab_size = 1
 
     if args.n_layer_to_finetune != -1:
         # then we are only finetuning n-th layer, not all the layers
