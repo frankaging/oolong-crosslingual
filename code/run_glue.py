c49c30acd9eb9f132c57a12fbeb53d06cee4a440
frankaging <zxw0424@gmail.com>
2021-11-07 12:30:15 -0800

    minor

--- code/run_glue.py
+++ code/run_glue.py
@@ -685,6 +685,7 @@ def main():
             )
             replacing_type_embeddings = random_model.roberta.embeddings.token_type_embeddings.weight.data.clone()
             replacing_type_embeddings[1] = model.roberta.embeddings.token_type_embeddings.weight.data[0]
+            
             # just swap the second one for randomly initialized weights.
             model.roberta.embeddings.token_type_embeddings.weight.data = replacing_type_embeddings
     
